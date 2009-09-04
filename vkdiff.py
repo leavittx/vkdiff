2ba90c3dc4d2385062d8903281c14efbeafb9b77
Lev <lev.panov@gmail.com>
2009-09-04 21:20:55 +0400

    removed 16th line due to Non-ASCII character bug

--- vkdiff.py
+++ vkdiff.py
@@ -13,7 +13,6 @@ g.setup(url='http://vkontakte.ru' + g.headers['Location'])
 g.request()
 g.setup(url='http://vkontakte.ru/friends.php')
 g.request()
-#list = re.findall(u'[0-9]{4,},"[А-Яа-яA-Za-z0-9]{10,}"',g.response_body)
 list = re.findall(u'[0-9]{4,},"',g.response_body)
 print list
 print len(list)
