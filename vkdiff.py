72c4e6c30cd17ee87541e40f2508075f7fca7e95
np3 <na5672@gmail.com>
2009-09-06 11:33:12 +0400

    rewrote script for using pda.vkontakte.ru

--- vkdiff.py
+++ vkdiff.py
@@ -6,13 +6,11 @@ from grab import Grab
 import re
 
 g = Grab()
-g.setup(url='http://vkontakte.ru/login.php',
+g.setup(url='http://pda.vkontakte.ru/login',
         post={'email' : 'george15@list.ru', 'pass' : 'coldplay15'})
 g.request()
-g.setup(url='http://vkontakte.ru' + g.headers['Location'])
+g.setup(url='http://pda.vkontakte.ru/write')
 g.request()
-g.setup(url='http://vkontakte.ru/friends.php')
-g.request()
-list = re.findall(u'[0-9]{4,},"',g.response_body)
+list = re.findall('<option value\="([0-9]+)" >',g.response_body)
 print list
 print len(list)
