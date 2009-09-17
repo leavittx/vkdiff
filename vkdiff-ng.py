93d5a01e547bb47da6eb1d2bb773df3b60020f48
np3 <na5672@gmail.com>
2009-09-18 00:21:20 +0400

    add names support

--- vkdiff-ng.py
+++ vkdiff-ng.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
- 
+
 from grab import Grab
 import re, getopt, sys, os
- 
+
 #insert your account info below =)
-email = ''
-password = ''
- 
+email = 'itmodermo@mail.ru'
+password = 'linuxa'
+
 HELPSTR = 'vkdiff by np3 & lp3, 2009\nUsage:\n-g, --get   - Get friends from web and write them to database\n\
 -d, --diff  - Diff friends from web and saved in database\n-p, --print - Print database\n\
 -h, --help  - Display this help'
- 
+
 DATAFILE = 'database'
- 
+
 def help():
   print HELPSTR
   
 def get():
 #!!!
-  #g.setup(url='http://vkontakte.ru/login.php', post={'email' : email, 'pass' : password}) 
-  g.setup(url='http://pda.vkontakte.ru/login', post={'email' : email, 'pass' : password})
+  g.setup(url='http://vkontakte.ru/login.php', post={'email' : email, 'pass' : password}) 
+  #g.setup(url='http://pda.vkontakte.ru/login', post={'email' : email, 'pass' : password})
 #!!!
   g.request()
 #!!!
-  #g.setup(url='http://vkontakte.ru/friends.php')
-  g.setup(url='http://pda.vkontakte.ru/write')
+  g.setup(url='http://vkontakte.ru/friends.php')
+  #g.setup(url='http://pda.vkontakte.ru/write')
 #!!!
   g.request()
   #udata = unicode(g.response_body).decode('utf-8')
 #!!!
-  #list = re.findall('\[(\d+),"([^"]*)', g.response_body)
-  list = re.findall('<option value\="([0-9]+)" >', g.response_body)
+  list = re.findall(u'\[(\d+),"([^"]*)', g.response_body)
+  #list = re.findall('<option value\="([0-9]+)" >', g.response_body)
 #!!!
   #print list
   return list
@@ -50,7 +50,7 @@ def writefile(buf, file):
 #      f.close()
 #    else:
       f = open(file, 'w')
-      f.write(buf)
+      f.write(buf.encode('utf-8'))
       f.close()
       
 def readfile(file):
@@ -64,9 +64,38 @@ def printbase():
     print readfile(DATAFILE)
     
 def diff():
+    R = []
     s1 = readfile(DATAFILE)
     b1 = s1.split('\n')
-    b2 = get()
+    for i in b1:
+     t = i.split(' ')
+     try:
+       R.append((t[0],t[1] + ' ' + t[2]))
+     except IndexError:
+       R.append((t[0],t[1]))
+    
+    b1 = dict(R)
+    b2 = dict(get())
+
+    
+
+    if len(b2) == 0:
+        print "Bad response from vkontakte.ru"
+        sys.exit(0)
+   # print b1.keys()
+   # print b2.keys()
+   #
+  #  print b2
+  #for i in b2:
+  #    L.append((i[0], i[1]))
+   # print L
+    #b1[:] = b1[:-1]
+ #   for i in b1:
+ #     t = i.split(' ')
+ #     R.append(t[0])
+    #print R
+    #sys.exit(0)
+    
     
 #    length = len(b1) - 1
 #    len2 = len(b2)
@@ -78,7 +107,7 @@ def diff():
 #      i += 1
 #      for i in range(i, length ):
 #            print "DIFF - " + b1[i] + ' ' + '(Empty)'
- 
+
 #   if len2 > length:
 #        for i in range(0, length ):
 #          if b1[i] != b2[i]:
@@ -86,15 +115,15 @@ def diff():
 #        i += 1
 #        for i in range(i, len2 ):
 #            print "DIFF - (Empty) " + b2[i]
- 
-    for item in b1:
-      if not item in b2:
-        print '- ' + item
- 
-    for item in b2:
-      if not item in b1:
-        print '+ ' + item
- 
+
+    for item in b2.keys():
+      if not item in b1.keys():
+        print '+ ' + b2[item]
+
+    for item in b1.keys():
+      if not item in b2.keys():
+        print '- ' + b1[item]
+     
 if __name__ == "__main__":
     g = Grab()
     newstr = ''
@@ -105,24 +134,18 @@ if __name__ == "__main__":
        help()
        sys.exit(0);
     if len(opts) == 0:
-        help()
+       # help()
+        diff()
         sys.exit()
     for o, a in opts:
         if o in ('-h', '--help'):
             help()
             sys.exit()
         elif o in ('-g', '--get'):
-#!!!            L = []
             l = get()
-#!!!            print l
-#            for i in l:
-#              L[:] = i[:]
-#              for j in L:
-#                print j.decode('utf-8')
-           # l[0][0].decode('utf-8') 
-            for s in l: #!!!
-                newstr += s + '\n' #!!!
-            
+            for i in l:
+              newstr += i[0] + ' ' + i[1] +  '\n'
+     
             writefile(newstr[:-1], DATAFILE) #!!!
         elif o in ('-p', '--print'):
             printbase()
